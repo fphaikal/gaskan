4b907b7269dac9fa4b69c1fbc6643101e24067a3
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-06-24 20:34:54 +0700

    update navbar item

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -45,9 +45,9 @@ const toggleMobileMenu = () => {
                   aria-current="page">Home</a>
               </li>
               <li>
-                <a href="team"
+                <a href="/view/siswa"
                   class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                  aria-current="page">Team</a>
+                  aria-current="page">Siswa</a>
               </li>
             </ul>
           </div>
