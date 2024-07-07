48d9e9e45713e99d8d57b1a22a4d97b8158d6888
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-07-07 16:03:16 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        // <meta name="google-site-verification" content="zGcUSdyFHTHDyelIHIQ-fAu2Ir0mG8xxa9b4ToiCiic" />
+        { name: 'google-site-verification', content: 'zGcUSdyFHTHDyelIHIQ-fAu2Ir0mG8xxa9b4ToiCiic' }
+      ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' }
+      ]
+    }
+  },
+  
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE,
@@ -9,6 +22,7 @@ export default defineNuxtConfig({
       buildId: process.env.VERCEL_GIT_COMMIT_SHA || 'development'
     },
   },
+  
   modules: [
     "@nuxtjs/tailwindcss",
     "@vueuse/nuxt",
