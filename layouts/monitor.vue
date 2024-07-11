914559221516f12f497a1ddc1e3d875658f0c206
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-07-11 21:42:03 +0700

    add page for monitor

---
+++ layouts/monitor.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <div class="min-h-screen">
+        <NuxtPage />
+      </div>
+    </div>
+  </template> 
---
+++ pages/monitor/right.vue
@@ -0,0 +1,29 @@
+<script setup>
+definePageMeta({
+  layout: 'monitor',
+})
+</script>
+<template>
+  <div class="bg-cover" style="background-image: url(https://tailwindcomponents.com/gradient-generator/assets/header.a6837f08.webp);">
+    <div class="flex items-center h-screen">
+      <div class="flex p-10 h-fit my-auto w-1/2">
+        <div class="bg-dark w-1/5 p-5">
+          <p class="text-xl flex flex-col font-bold justify-center h-full">2024-07-10</p>
+        </div>
+        <div class="bg-dark2 w-4/5 p-5">
+          <p>22100697</p>
+          <p class="text-2xl font-bold">NAIRA SALIMA</p>
+        </div>
+      </div>
+      <div class="flex p-10 h-fit my-auto w-1/2">
+        <div class="bg-dark w-1/5 p-5">
+          <p class="text-xl flex flex-col font-bold justify-center h-full">2024-07-10</p>
+        </div>
+        <div class="bg-dark2 w-4/5 p-5">
+          <p>22100697</p>
+          <p class="text-2xl font-bold">FAHREZA PASHA HAIKAL</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
