918ff1d237b42148b273822d3b17f8fc80ce4b39
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-07-16 12:24:33 +0700

    update monitor

--- pages/monitor/left.vue
+++ pages/monitor/left.vue
@@ -6,24 +6,35 @@ definePageMeta({
 <template>
   <div class="bg-cover" style="background-image: url(https://tailwindcomponents.com/gradient-generator/assets/header.a6837f08.webp);">
     <div class="flex items-center h-screen">
-      <div class="flex p-10 h-fit my-auto w-1/2">
-        <div class="bg-dark w-1/5 p-5">
-          <p class="text-xl flex flex-col font-bold justify-center h-full">2024-07-10</p>
-        </div>
-        <div class="bg-dark2 w-4/5 p-5">
-          <p>22100697</p>
+      <div class="flex flex-col p-5 h-fit my-auto w-1/2">
+        <div class="bg-dark2 p-5">
+          <p>22100579</p>
           <p class="text-2xl font-bold">NAIRA SALIMA</p>
         </div>
-      </div>
-      <div class="flex p-10 h-fit my-auto w-1/2">
-        <div class="bg-dark w-1/5 p-5">
-          <p class="text-xl flex flex-col font-bold justify-center h-full">2024-07-10</p>
+        <div class="flex">
+          <div class="bg-dark px-5 py-2 w-5/6">
+            <p class="text-xl font-semibold ">2024-07-10</p>
+          </div>
+          <div class="bg-success px-5 py-2 w-3/6">
+            <p class="text-xl font-semibold flex justify-end">Tepat Waktu</p>
+          </div>
         </div>
-        <div class="bg-dark2 w-4/5 p-5">
+      </div>
+      <div class="flex flex-col p-5 h-fit my-auto w-1/2">
+        <div class="bg-dark2 p-5">
           <p>22100697</p>
           <p class="text-2xl font-bold">FAHREZA PASHA HAIKAL</p>
         </div>
+        <div class="flex">
+          <div class="bg-dark px-5 py-2 w-5/6">
+            <p class="text-xl font-semibold ">2024-07-10</p>
+          </div>
+          <div class="bg-success px-5 py-2 w-3/6">
+            <p class="text-xl font-semibold flex justify-end">Tepat Waktu</p>
+          </div>
+        </div>
       </div>
+      
     </div>
   </div>
 </template>
--- pages/monitor/right.vue
+++ pages/monitor/right.vue
@@ -6,12 +6,12 @@ definePageMeta({
 <template>
   <div class="bg-cover" style="background-image: url(https://tailwindcomponents.com/gradient-generator/assets/header.a6837f08.webp);">
     <div class="flex items-center h-screen">
-      <div class="flex p-10 h-fit my-auto w-1/2">
+      <div class="flex p-5 h-fit my-auto w-1/2">
         <div class="bg-dark w-1/5 p-5">
           <p class="text-xl flex flex-col font-bold justify-center h-full">2024-07-10</p>
         </div>
         <div class="bg-dark2 w-4/5 p-5">
-          <p>22100697</p>
+          <p>22100579</p>
           <p class="text-2xl font-bold">NAIRA SALIMA</p>
         </div>
       </div>
