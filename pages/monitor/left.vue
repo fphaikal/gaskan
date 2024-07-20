6658dd7b3c2731ad1cc37a4dcfb36985f17b3a65
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-07-20 14:25:22 +0700

    update monitor page

--- pages/monitor/left.vue
+++ pages/monitor/left.vue
@@ -35,15 +35,14 @@ onUnmounted(() => {
 <template>
   <div class="bg-cover" style="background-image: url(https://tailwindcomponents.com/gradient-generator/assets/header.a6837f08.webp);">
     <div class="flex items-center h-screen">
-      <div v-if="log.lf1" class="flex flex-col p-5 h-fit my-auto w-1/2">
-        <img :src="log.lf1.Image" alt="">
-        <div class="bg-dark2 p-5">
+      <div v-if="log.lf1" class="flex flex-col p-5 h-fit my-auto w-1/2 items-center">
+        <div class="bg-dark2 p-5 w-full">
           <p>{{ log.lf1.NIS }} | {{ log.lf1.Kelas }}</p>
           <p class="text-2xl font-bold">{{ log.lf1.Nama }}</p>
         </div>
-        <div class="flex">
+        <div class="flex w-full">
           <div class="bg-dark px-5 py-2 w-5/6">
-            <p class="text-xl font-semibold ">{{ formatLongDate(log.lf1.timestamp) }}</p>
+            <p class="text-xl font-semibold ">{{ new Date(log.lf1.timestamp).toLocaleString('id-ID') }}</p>
           </div>
           <div v-if="log.lf1.indexTelat === false" class="bg-success px-5 py-2 w-3/6">
             <p class="text-xl font-semibold flex justify-end">Tepat Waktu</p>
@@ -52,16 +51,16 @@ onUnmounted(() => {
             <p class="text-xl font-semibold flex justify-end">Terlambat</p>
           </div>
         </div>
+        <img :src="log.lf1.Image" class="w-2/3 flex justify-center items-center mt-10" alt="">
       </div>
-      <div v-if="log.lf2" class="flex flex-col p-5 h-fit my-auto w-1/2">
-        <img :src="log.lf1.Image" alt="">
-        <div class="bg-dark2 p-5">
+      <div v-if="log.lf2" class="flex flex-col p-5 h-fit my-auto w-1/2 items-center">
+        <div class="bg-dark2 p-5 w-full">
           <p>{{ log.lf2.NIS }} | {{ log.lf2.Kelas }}</p>
           <p class="text-2xl font-bold">{{ log.lf2.Nama }}</p>
         </div>
-        <div class="flex">
+        <div class="flex w-full">
           <div class="bg-dark px-5 py-2 w-5/6">
-            <p class="text-xl font-semibold ">{{ formatLongDate(log.lf2.timestamp) }}</p>
+            <p class="text-xl font-semibold ">{{ new Date(log.lf2.timestamp).toLocaleString('id-ID') }}</p>
           </div>
           <div v-if="log.lf2.indexTelat === false" class="bg-success px-5 py-2 w-3/6">
             <p class="text-xl font-semibold flex justify-end">Tepat Waktu</p>
@@ -70,8 +69,8 @@ onUnmounted(() => {
             <p class="text-xl font-semibold flex justify-end">Terlambat</p>
           </div>
         </div>
+        <img :src="log.lf1.Image" class="w-2/3 flex justify-center items-center mt-10" alt="">
       </div>
-      
     </div>
   </div>
 </template>
--- pages/monitor/right.vue
+++ pages/monitor/right.vue
@@ -7,7 +7,7 @@ const log = ref([]);
 const socket = ref(null);
 
 onMounted(() => {
-  socket.value = new WebSocket('wss://api.tierkun.my.id/viewlf1');
+  socket.value = new WebSocket('wss://api.tierkun.my.id/viewlf3');
 
   socket.value.onopen = () => {
     console.log('Connected to WebSocket server');
@@ -35,43 +35,42 @@ onUnmounted(() => {
 <template>
   <div class="bg-cover" style="background-image: url(https://tailwindcomponents.com/gradient-generator/assets/header.a6837f08.webp);">
     <div class="flex items-center h-screen">
-      <div v-if="log.lf1" class="flex flex-col p-5 h-fit my-auto w-1/2 items-center">
+      <div v-if="log.lf3" class="flex flex-col p-5 h-fit my-auto w-1/2 items-center">
         <div class="bg-dark2 p-5 w-full">
-          <p>{{ log.lf1.NIS }} | {{ log.lf1.Kelas }}</p>
-          <p class="text-2xl font-bold">{{ log.lf1.Nama }}</p>
+          <p>{{ log.lf3.NIS }} | {{ log.lf3.Kelas }}</p>
+          <p class="text-2xl font-bold">{{ log.lf3.Nama }}</p>
         </div>
         <div class="flex w-full">
           <div class="bg-dark px-5 py-2 w-5/6">
-            <p class="text-xl font-semibold ">{{ formatLongDate(log.lf1.timestamp) }}</p>
+            <p class="text-xl font-semibold ">{{ new Date(log.lf3.timestamp).toLocaleString('id-ID') }}</p>
           </div>
-          <div v-if="log.lf1.indexTelat === false" class="bg-success px-5 py-2 w-3/6">
+          <div v-if="log.lf3.indexTelat === false" class="bg-success px-5 py-2 w-3/6">
             <p class="text-xl font-semibold flex justify-end">Tepat Waktu</p>
           </div>
           <div v-else class="bg-danger px-5 py-2 w-3/6">
             <p class="text-xl font-semibold flex justify-end">Terlambat</p>
           </div>
         </div>
-        <img :src="log.lf1.Image" class="w-2/3 flex justify-center items-center mt-10" alt="">
+        <img :src="log.lf3.Image" class="w-2/3 flex justify-center items-center mt-10" alt="">
       </div>
-      <div v-if="log.lf2" class="flex flex-col p-5 h-fit my-auto w-1/2 items-center">
+      <div v-if="log.lf4" class="flex flex-col p-5 h-fit my-auto w-1/2 items-center">
         <div class="bg-dark2 p-5 w-full">
-          <p>{{ log.lf2.NIS }} | {{ log.lf2.Kelas }}</p>
-          <p class="text-2xl font-bold">{{ log.lf2.Nama }}</p>
+          <p>{{ log.lf4.NIS }} | {{ log.lf4.Kelas }}</p>
+          <p class="text-2xl font-bold">{{ log.lf4.Nama }}</p>
         </div>
         <div class="flex w-full">
           <div class="bg-dark px-5 py-2 w-5/6">
-            <p class="text-xl font-semibold ">{{ formatLongDate(log.lf2.timestamp) }}</p>
+            <p class="text-xl font-semibold ">{{ new Date(log.lf4.timestamp).toLocaleString('id-ID') }}</p>
           </div>
-          <div v-if="log.lf2.indexTelat === false" class="bg-success px-5 py-2 w-3/6">
+          <div v-if="log.lf4.indexTelat === false" class="bg-success px-5 py-2 w-3/6">
             <p class="text-xl font-semibold flex justify-end">Tepat Waktu</p>
           </div>
           <div v-else class="bg-danger px-5 py-2 w-3/6">
             <p class="text-xl font-semibold flex justify-end">Terlambat</p>
           </div>
         </div>
-        <img :src="log.lf1.Image" class="w-2/3 flex justify-center items-center mt-10" alt="">
+        <img :src="log.lf3.Image" class="w-2/3 flex justify-center items-center mt-10" alt="">
       </div>
-      
     </div>
   </div>
 </template>
