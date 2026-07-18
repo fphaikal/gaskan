040eb4f52afcf151ef9116c97f7aec5157872487
Muhammad Tier Sinyo C.U.S. <118105588+Stalker-moment@users.noreply.github.com>
2026-07-18 21:32:34 +0700

    Update team.vue

--- pages/team.vue
+++ pages/team.vue
@@ -42,7 +42,7 @@ const groupedTeam = computed(() => {
   if (pembimbing.length > 0) {
     result.push({
       title: 'Pembimbing',
-      icon: 'mingcute:academic-cap-fill',
+      icon: 'mingcute:academic-2-fill',
       members: pembimbing
     })
   }
